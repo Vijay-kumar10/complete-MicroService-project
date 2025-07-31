fa8215a51bb41f0ec7d30a3bc52640795bfa30ec
Vijay-kumar10 <kumar888985@gmail.com>
2025-07-31 18:45:20 +0530

    Implementation of Retry annotation

--- UserService/src/main/java/com/userservice/controller/UserController.java
+++ UserService/src/main/java/com/userservice/controller/UserController.java
@@ -4,6 +4,8 @@ import com.userservice.entities.User;
 import com.userservice.service.UserService;
 import feign.Response;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,24 +28,29 @@ public class  UserController {
     }
 
 
+    int retryCount = 1;
     //get single user
     @GetMapping("/{userId}")
-    @CircuitBreaker(name = "hotelRatingBreaker",fallbackMethod = "ratingHotelFallBack")
+//    @CircuitBreaker(name = "hotelRatingBreaker",fallbackMethod = "ratingHotelFallBack")
+    @Retry(name = "hotelRatingRetryBreaker",fallbackMethod = "ratingHotelFallBack")
+
     public ResponseEntity<User> getUser(@PathVariable String userId ){
+        System.out.println("Retry count : " + retryCount++);
         User user = userService.getUserById(userId);
         return ResponseEntity.ok(user);
     }
-
     //jab bhi hum kisi function ka fallback function banate hai tab un dono ka input variable and return types same hona chiaye nahi to exception aa jati hai
-   public ResponseEntity<User> ratingHotelFallBack(String userId,Exception e) {
-       System.out.println("Fallback funciton is executed because some service are down : "+e.getMessage());
-       User dummyUser = User.builder().id("12345")
-               .email("dummy@email.com")
-               .name("dummy user")
-               .about("This is a dummy user.")
-               .build();
-       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(dummyUser);
-   }
+    public ResponseEntity<User> ratingHotelFallBack(String userId,Exception e) {
+        System.out.println("Fallback funciton is executed because some service are down : "+e.getMessage());
+        User dummyUser = User.builder().id("12345")
+                .email("dummy@email.com")
+                .name("dummy user")
+                .about("This is a dummy user.")
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(dummyUser);
+    }
+
+
 
     //get all users
     @GetMapping
